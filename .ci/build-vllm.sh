ab158b44e4406fedd4d67abe8d821a2b835f1864
GolfRabbit <lvyiwen@gmail.com>
2026-06-08 22:46:30 +1200

    Change base image version from ubuntu22.04 to ubuntu20.04

--- .ci/build-vllm.sh
+++ .ci/build-vllm.sh
@@ -61,7 +61,7 @@ docker build \
   --build-arg "torch_cuda_arch_list=6.0 6.1" \
   --build-arg "max_jobs=2" \
   --build-arg "nvcc_threads=2" \
-  --build-arg "BUILD_BASE_IMAGE=docker.io/nvidia/cuda:$CUDA_VERSION-devel-ubuntu22.04" \
+  --build-arg "BUILD_BASE_IMAGE=docker.io/nvidia/cuda:$CUDA_VERSION-devel-ubuntu20.04" \
   --file "$dockerfile" \
   --output "type=tar,dest=$root/tmp/build.tar" \
   --secret "id=SETUPTOOLS_SCM_PRETEND_VERSION_FOR_VLLM" \
