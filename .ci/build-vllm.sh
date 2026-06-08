a5c88adef2c75b603133a2a04452928d6b2ba308
GolfRabbit <lvyiwen@gmail.com>
2026-06-08 23:12:40 +1200

    Update CUDA version and base image in build script

--- .ci/build-vllm.sh
+++ .ci/build-vllm.sh
@@ -9,7 +9,7 @@ repository="$2"
 ref="$3"
 ghcr_user="$4"
 ghcr_token="$5"
-CUDA_VERSION=${6:-"12.1.0"}
+CUDA_VERSION=${6:-"12.8.1"}
 
 # Use root directory
 cd "$root"
@@ -61,7 +61,7 @@ docker build \
   --build-arg "torch_cuda_arch_list=6.0 6.1" \
   --build-arg "max_jobs=2" \
   --build-arg "nvcc_threads=2" \
-  --build-arg "BUILD_BASE_IMAGE=docker.io/nvidia/cuda:$CUDA_VERSION-devel-ubuntu20.04" \
+  --build-arg "BUILD_BASE_IMAGE=docker.io/nvidia/cuda:$CUDA_VERSION-devel-ubuntu22.04" \
   --file "$dockerfile" \
   --output "type=tar,dest=$root/tmp/build.tar" \
   --secret "id=SETUPTOOLS_SCM_PRETEND_VERSION_FOR_VLLM" \
